46f3d653f1365ac3821538dc90029bd427b88b85
Andrei Deak <andrei.deak@icloud.com>
2026-04-06 03:12:59 +0200

    add --verbose flag for output with token metrics

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import os
 import argparse
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
 
 def main():
     # Load environment variables
@@ -16,21 +17,25 @@ def main():
     # Use argparser to extract question from command argument
     parser = argparse.ArgumentParser(description="Simon Code")
     parser.add_argument("user_prompt", type=str, help="User prompt")
+    parser.add_argument("--verbose", action="store_true", help="Enable verbose output")
     args = parser.parse_args()
 
+    # Store chat conversation
+    messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
+
     user_prompt = "Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
-    response = client.models.generate_content(model="gemini-2.5-flash", contents=args.user_prompt)
+    response = client.models.generate_content(model="gemini-2.5-flash", contents=messages)
     
     if response.usage_metadata == None:
         raise Exception(RuntimeError)
     
-    prompt_tokens = response.usage_metadata.prompt_token_count
-    response_tokens = response.usage_metadata.candidates_token_count
-
-    print(f"User prompt: {args.user_prompt}")
-    print(f"Prompt tokens: {prompt_tokens}")
-    print(f"Response tokens: {response_tokens}")
-    print(f"Response: \n{response.text}")
+    if args.verbose:
+        print(f"User prompt: {args.user_prompt}")
+        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+        print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+        print(f"Response: \n{response.text}")
+    else:
+        print(f"Response: \n{response.text}")
 
 
 if __name__ == "__main__":
