5802f34d164d37f12b37babd6e7e82d0b8e06d9f
Andrei Deak <andrei.deak@icloud.com>
2026-04-06 02:49:15 +0200

    add token usage metrics

--- main.py
+++ main.py
@@ -10,8 +10,20 @@ def main():
     
     client = genai.Client(api_key=api_key)
 
-    response = client.models.generate_content(model="gemini-2.5-flash", contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.")
-    print(response.text)
+    user_prompt = "Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
+    response = client.models.generate_content(model="gemini-2.5-flash", contents=user_prompt)
+    
+    if response.usage_metadata == None:
+        raise Exception(RuntimeError)
+    
+    prompt_tokens = response.usage_metadata.prompt_token_count
+    response_tokens = response.usage_metadata.candidates_token_count
+
+    print(f"User prompt: {user_prompt}")
+    print(f"Prompt tokens: {prompt_tokens}")
+    print(f"Response tokens: {response_tokens}")
+    print(f"Response: \n{response.text}")
+
 
 if __name__ == "__main__":
     main()
